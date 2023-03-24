3138b4b9af25c40422980b0875494affab626feb
James Niyongira <lascasasjames85@gmail.com>
2023-03-23 22:16:26 -0400

    added Account class

---
+++ Account.cs
@@ -0,0 +1,7 @@
+namespace Bank
+{
+    public class Account
+    {
+
+    }
+}
---
+++ Bank.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bank
+{
+    public class Bank
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Customer Name: ");
+            string name = Console.ReadLine();
+
+        }
+    }
+}
